ac3a42a779616a8ab99ae8bfae1e089512e3d780
VeetiT <n3tive00@students.oamk.fi>
2025-10-01 12:56:48 +0300

    Lisätty vastaukset ja nappi millä ne saa näkyviin

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,11 @@
+function showAnswers() {
+  const answers = document.querySelectorAll(".answer");
+  answers.forEach(answer => {
+    if (answer.style.display === "none" || answer.style.display === "") {
+      answer.style.display = "block";
+    } else {
+      answer.style.display = "none";
+    }
+  });
+}
 
